18e12c8e95adf717bf7bd4d1b2ab16d64f5277a4
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-04 13:28:20 -0700

    adding style to the button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'; 
 import MyNewComponent from "./newcomponent/MyNewComponent"
 
 import './App.css';
--- src/newcomponent/MyNewComponent.js
+++ src/newcomponent/MyNewComponent.js
@@ -1,4 +1,6 @@
-import React,{Component}from 'react'
+import React,{Component}from 'react';
+import './MyNewComponent.css';
+
 class MyNewComponent extends Component {
     constructor(props){
         super(props);
@@ -19,7 +21,8 @@ class MyNewComponent extends Component {
                 <p>Age: {this.state.age}</p>
                <p> HairColor: {this.props.hairColor}</p>
             
-            <button onClick={this.ageInc}>Happy Birthday {this.props.firstName} {this.props.lastName}</button>
+            <button className='btn' onClick={this.ageInc}>Happy Birthday {this.props.firstName} {this.props.lastName}</button>
+            
             </div>
 
 
