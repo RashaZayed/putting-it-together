4c38cf2f846bab3e63df7470a6944ab87b04f5ce
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-03 23:05:41 -0700

    person card with birthday button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import MyNewComponent from "./newcomponent/MyNewComponent"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MyNewComponent lastName="Zayed" firstName="Rasha" age={30} hairColor="Brown"/>
+     <MyNewComponent lastName="Smith" firstName="Doe" age={40}hairColor="Brown"/>
+     <MyNewComponent lastName="Elder" firstName="Augisanda" age={50} hairColor="Brown"/>
+     <MyNewComponent lastName="Rendell" firstName="Moh" age={60} hairColor="Black"/>
     </div>
   );
 }
---
+++ src/newcomponent/MyNewComponent.js
@@ -0,0 +1,31 @@
+import React,{Component}from 'react'
+class MyNewComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            age: this.props.age
+        }
+    }
+    ageInc =()=> {
+        this.setState({ age: this.state.age + 1 });
+    
+     }
+    
+
+    render(){
+        return(
+            <div>
+               <h1> {this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+               <p> HairColor: {this.props.hairColor}</p>
+            
+            <button onClick={this.ageInc}>Happy Birthday {this.props.firstName} {this.props.lastName}</button>
+            </div>
+
+
+
+
+        )
+    }
+}
+export default MyNewComponent
